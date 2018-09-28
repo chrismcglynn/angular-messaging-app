b0562b8f1b316f9e0d172457495a25d28488ea21
chrismcglynn <chris@mcglynnenterprises.com>
2018-09-27 21:08:43 -0400

    set up the posts route

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    next();
-});
+app.use("/api/posts", (req, res, next) => {
+    const posts = [
+        {
+            id: 'abcdef1234',
+            title: 'First server-side post',
+            content: 'This is coming from the server'
+        },
+        {
+          id: 'abcdef1235',
+          title: 'Second server-side post',
+          content: 'This is also coming from the server'
+        }
+    ];
 
-app.use((req, res, next) => {
-
+    res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
